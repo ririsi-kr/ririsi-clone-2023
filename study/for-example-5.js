1de16f3f80d66ef87b1f88c1aa472f970e2720ef
ririsi-kr <ririsi.kr@gmail.com>
2023-01-18 12:33:12 +0900

    restore

---
+++ study/for-example-5.js
@@ -0,0 +1,9 @@
+function main() {
+  const fruits = ["사과", "귤", "수박", "메론", "포도", "체리"];
+
+  for (let i = 0; i < 6; i++) {
+    console.log(fruits[i]);
+  }
+}
+
+main();
---
+++ study/for-example-quiz-1.js
@@ -0,0 +1,9 @@
+function main() {
+  const fruits = ["사과", "귤", "수박", "메론", "포도", "체리"];
+
+  console.log("과일 길이는?:");
+}
+
+main();
+
+// 과일 길이를 출력하는 방법은?(array 관련 내용 복습)
